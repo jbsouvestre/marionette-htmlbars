4fbd5c81bea32990280a7c299f0e33757bd636fe
jb <jb@remotium.com>
2015-10-23 15:08:49 +0200

    editor style was messed up

--- src/HTMLBarsView.js
+++ src/HTMLBarsView.js
@@ -4,13 +4,13 @@ import render from './render';
 Renderer.render = render;
 
 const HTMLBarsView = LayoutView.extend({
-	attachElContent(node) {
-		if(node){
-			this.$el.html(node);
-			return this;
-		}
-		return false;
-	}
+    attachElContent(node) {
+        if(node){
+            this.$el.html(node);
+            return this;
+        }
+        return false;
+    }
 });
 
 export default HTMLBarsView;
